1359941362d8ce6caef45d6466cdaeac461c59b6
Olga Mitiugova <olga.mityugova@gmail.com>
2018-10-04 20:25:05 +0300

    improve layout

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const del  = require('del');
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const { events } = require('./events.json')
+
+const destDir = 'public';
+
+function errorHandling(error) {
+    console.log(error.message);
+    this.emit('end');
+}
+
+gulp.task('clean', () => {
+    return del.sync(destDir + '/*', {force: true});
+});
+
+gulp.task('css', () => {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass())
+        .on('error', errorHandling)
+        .pipe(gulp.dest(destDir + '/css'));
+});
+
+gulp.task('assets', () => {
+    return gulp.src('assets/**/*')
+        .pipe(gulp.dest(destDir + '/assets'));
+});
+
+gulp.task('pug', () => {
+    return gulp.src('view/**/*.pug')
+
+        .pipe(pug({
+            pretty: true,
+            data: {
+                events: JSON.stringify(events)
+                //events
+            }
+        }))
+        .on('error', errorHandling)
+        .pipe(gulp.dest(destDir));
+});
+
+gulp.task('watch', ['default'], () => {
+    gulp.watch('scss/**/*.scss', ['css']);
+    gulp.watch('view/**/*.pug', ['pug']);
+    gulp.watch('assets/**/*', ['assets']);
+});
+
+gulp.task('default', ['clean', 'pug', 'css', 'assets']);
